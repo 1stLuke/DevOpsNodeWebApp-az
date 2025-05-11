b4d096e1d181da8cd0d5e541354e010abfbb2044
1stLuke <lukas.barth8@gmail.com>
2025-05-11 23:53:17 +0200

    Changed server.js once again

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Github-Actions-Pipeline1');
+  res.send('Github-Actions-Pipeline2');
 });
 
 app.listen(PORT, HOST);
